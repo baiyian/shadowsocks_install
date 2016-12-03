3f143b82c16ede7a93677915f830c1a4e45b2ea5
lbbboy <lbbboy@gmail.com>
2016-12-03 17:48:11 +0800

    Update shadowsocksR.sh

--- shadowsocksR.sh
+++ shadowsocksR.sh
@@ -196,12 +196,12 @@ download_files(){
     fi
     # Download ShadowsocksR init script
     if check_sys packageManager yum; then
-        if ! wget --no-check-certificate https://github.com/lbbboy/shadowsocks_install/edit/master/shadowsocksR -O /etc/init.d/shadowsocks; then
+        if ! wget --no-check-certificate https://raw.githubusercontent.com/lbbboy/shadowsocks_install/master/shadowsocksR -O /etc/init.d/shadowsocks; then
             echo "Failed to download ShadowsocksR chkconfig file!"
             exit 1
         fi
     elif check_sys packageManager apt; then
-        if ! wget --no-check-certificate https://github.com/lbbboy/shadowsocks_install/edit/master/shadowsocksR-debian -O /etc/init.d/shadowsocks; then
+        if ! wget --no-check-certificate https://raw.githubusercontent.com/lbbboy/shadowsocks_install/master/shadowsocksR-debian -O /etc/init.d/shadowsocks; then
             echo "Failed to download ShadowsocksR chkconfig file!"
             exit 1
         fi
